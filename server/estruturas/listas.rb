0517f12d3be259cbe547f170324e99178f33f97d
raphael <rafavfx1@gmail.com>
2024-03-06 23:38:23 -0300

    limitar a visualizacao

--- server/estruturas/listas.rb
+++ server/estruturas/listas.rb
@@ -2,9 +2,11 @@ lista = []
 
 lista.push(1,2,3,4,5,6,7,8,9,10)
 
-puts lista
+# puts lista
 # print lista[0]
 
 lista.insert(10, 11) ## adicionando no campo [10] da lista o valor -> 11
 
-print lista
+# print lista
+
+print lista[0..5] # visualizar do primeiro elemento até o 6o
