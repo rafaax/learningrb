e540dd766dd497d25598f31508782966abbb0cae
raphael <rafavfx1@gmail.com>
2024-03-06 22:11:14 -0300

    substrring

--- server/strip_slice.rb
+++ server/strip_slice.rb
@@ -5,5 +5,5 @@ puts "   TEST   ".strip
 # slicing de string
 string = "TTTTeste"
 puts string
-string[0..2] = "" # corta as 3 primeiras letras -> 0,1,2 da variavel
+string[0..2] = "" # corta as 3 primeiras letras -> 0,1,2 da variavel -> substring
 print string
