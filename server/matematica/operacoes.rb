4ac59040456596890bbd469795d8740baadee8b4
raphael <rafavfx1@gmail.com>
2024-03-06 22:36:25 -0300

    separando por pastas

---
+++ server/matematica/operacoes.rb
@@ -0,0 +1,32 @@
+## operacoes simples matematicas
+
+soma = 10 + 10
+subtracao = 30 -10
+divisao = 200 / 10
+multiplicacao = 2 * 10
+
+
+puts soma
+puts subtracao
+puts divisao
+puts multiplicacao
+
+
+# passando com variavel]
+
+puts "COM VARIAVEL X E Y"
+
+puts "Digite um valor para X"
+x = gets.chomp.to_f
+puts "Digite um valor para Y"
+y = gets.chomp.to_f
+
+soma = x + y
+subtracao = x - y
+divisao = x / y
+multiplicacao = x * y
+
+puts soma
+puts subtracao
+puts divisao
+puts multiplicacao
