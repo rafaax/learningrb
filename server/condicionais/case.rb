951e7df84c150335048f8c2cc0153dd0d784d973
raphael <rafavfx1@gmail.com>
2024-03-06 22:53:17 -0300

    case, else if, if

---
+++ server/condicionais/case.rb
@@ -0,0 +1,20 @@
+letra_alfabeto = 1 # IR ALTERANDO NUM
+
+case letra_alfabeto
+when 1
+  puts 'A'
+when 2
+  puts 'B'
+when 2
+  puts 'B'
+when 3
+  puts 'C'
+when 4
+  puts 'D'
+when 5
+  puts 'E'
+when 6
+  puts 'F'
+when 7
+  puts 'G'
+end
---
+++ server/condicionais/elseif.rb
@@ -0,0 +1,10 @@
+# ruby else if é elsif
+peso = gets.to_f
+
+if peso > 50
+  puts "Maior que 50"
+elsif peso < 50
+  puts "Menor que 50"
+else
+  puts "Peso é 50"
+end
