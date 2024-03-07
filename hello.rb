61a524fb2a29745ece9fd8ce1968ceb45e96ebeb
raphael <rafavfx1@gmail.com>
2024-03-06 21:35:25 -0300

    interpolacao de string e variaveis

--- hello.rb
+++
@@ -1,2 +0,0 @@
-print "essa string continuará na mesma linha"  # print - mantem na mesma linha
-puts "essa string irá pular até a proxima linha"  # puts - pula p/ proxima linha
---
+++ strings.rb
@@ -0,0 +1,12 @@
+print "essa string continuará na mesma linha"  # print - mantem na mesma linha
+puts "essa string irá pular até a proxima linha"  # puts - pula p/ proxima linha
+
+numero_x = 1;
+numero_y = 7;
+
+print numero_x
+print numero_y
+
+## interpolacao de string
+puts "o numero x é #{numero_x}" 
+puts "o numero y é #{numero_y}"
