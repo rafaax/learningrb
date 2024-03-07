d0260d3a27bdd445e24b39d85558ebedf5468f3d
raphael <rafavfx1@gmail.com>
2024-03-06 23:42:41 -0300

    delete - insert e length

--- server/estruturas/listas.rb
+++ server/estruturas/listas.rb
@@ -2,11 +2,14 @@ lista = []
 
 lista.push(1,2,3,4,5,6,7,8,9,10)
 
-# puts lista
-# print lista[0]
+puts lista[0]  # printa apenas o primeiro elemento da lista
 
 lista.insert(10, 11) ## adicionando no campo [10] da lista o valor -> 11
 
-# print lista
 
 print lista[0..5] # visualizar do primeiro elemento até o 6o
+puts # printa um espaço vazio no console
+
+lista.delete(11) # deletando o valor 11
+
+puts lista.length # numero de elementos dentro da lista
