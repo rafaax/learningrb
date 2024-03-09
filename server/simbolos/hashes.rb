039e4dcd2f80f13c063ee3499b5dfbf178f51e7c
raphael <rafavfx1@gmail.com>
2024-03-08 21:23:13 -0300

    hashes

---
+++ server/simbolos/hashes.rb
@@ -0,0 +1,10 @@
+## dicionarios
+
+hash = {}
+hash_via_object = Hash.new
+
+puts hash, hash_via_object
+
+
+hash = {nome: "Rapha", peso: 66.1, idade: 19}
+hash[:altura] = 1.71
--- server/simbolos/simbolos.rb
+++ server/simbolos/simbolos.rb
@@ -1,1 +1,6 @@
 puts :id.object_id
+puts :nome.object_id
+puts :nome.object_id #nao cria outra alocacao de memoria para o mesmo simbolo
+
+hashes = {nome: "Raphael", idade: 19}
+puts hashes
