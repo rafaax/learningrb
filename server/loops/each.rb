d6945527d482d8ab98f91d0a4147b51a7539a9cf
raphael <rafavfx1@gmail.com>
2024-03-08 22:18:18 -0300

    foreach com dicionario

--- server/loops/each.rb
+++ server/loops/each.rb
@@ -1,7 +1,24 @@
 # foreach
 
+puts "Com vetor"
+puts
 frutas = ['Banana','Uva','Manga', 'Pera']
 
 frutas.each do |frutas|
   puts frutas
+end
+
+
+
+
+dict = Hash.new
+dict = {produto: "Boné", preco: 30, quantidade: 7}
+produtos = dict
+
+
+puts "Com dicionario"
+puts
+
+produtos.each do |k, v|
+  puts "#{k} => #{v}"
 end
