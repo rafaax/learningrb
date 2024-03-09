400d9184d30e0dcae89073174998e4546092e159
raphael <rafavfx1@gmail.com>
2024-03-08 22:32:54 -0300

    funcoes / metodos

---
+++ server/funcoes/funcoes.rb
@@ -0,0 +1,15 @@
+def somar num1, num2
+  return num1 + num2
+end
+
+def sub(num1, num2)
+  return num1 - num2
+end
+
+x = 10
+y = 3
+
+soma = somar(x, y)
+subtracao = sub(x, y)
+
+puts soma, subtracao
